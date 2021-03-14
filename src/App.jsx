eee62830cce9b910aec1e1c2d5b987bb01117c86
Luís Fernando da Silveira <luisfernandosilveira23@gmail.com>
2021-03-14 17:23:03 -0300

    Projeto de petshop com posts e uso de react-router-dom

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./assets/css/base/base.css";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Home from "./paginas/Home";
+import Sobre from "./paginas/Sobre";
+import Pagina404 from "./paginas/Pagina404";
+import Cabecalho from "./components/Cabecalho";
+import Post from "./paginas/Post";
+import Categoria from "./paginas/Categoria";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Cabecalho />
+      <Switch>
+        <Route path="/" component={Home} exact />
+        <Route path="/sobre" component={Sobre} />
+        <Route path="/categoria/:id" component={Categoria} />
+        <Route path="/posts/:id" component={Post} />
+        <Route component={Pagina404} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default App;
